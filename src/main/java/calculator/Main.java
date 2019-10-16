4c616dfe73508509d77f148a4ee7031ff0f8b320
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2019-10-16 12:43:33 -0500

    Final commit

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,10 +1,15 @@
 package calculator;
 
+import java.util.*;
+
 public class Main {
     public static void main(String args[]){
         System.out.println("Calculator");
 
-		EquationSolver equationSolver = new EquationSolver("2 + 2");
+		Scanner s = new Scanner(System.in);
+		String eq = s.nextLine();
+
+		EquationSolver equationSolver = new EquationSolver(s);
         System.out.println(equationSolver.operate());
     }
 }
